ddab40be81276988dc7ef7915e20e7a0c175860a
Connor Buckley <connorbuckley144@gmail.com>
2025-07-16 00:33:03 -0700

    Navbar css, project page loading text

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,19 +4,21 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="bg-blue-800 text-white">
-      <div className="flex justify-between items-center h-16 px-4 sm:px-8 lg:px-10">
+    <nav className="sticky top-0 z-50 text-xl font-inter bg-gradient-to-r from-cyan-600 to-blue-500 bg-white shadow-md text-white">
+      <div className="flex justify-between items-center h-16 px-4 sm:px-8 lg:px-14">
         {/* Logo / Site Name */}
-        <div className="text-lg font-bold">
-          <Link to="/">Construction Company</Link>
-        </div>
+        <Link to="/" className="flex items-center gap-2 font-bold">
+          <img src="/img/logo_placeholder.png" className="w-10 h-10 object-contain" alt="Your Image" />
+          <p className="px-2">Construction Company</p>
+        </Link>
+
 
         {/* Nav Links */}
-        <div className="space-x-4">
+        <div className="space-x-8">
           <Link to="/" className="hover:text-gray-300">Home</Link>
           <Link to="/projects" className="hover:text-gray-300">Projects</Link>
-          <Link to="/about" className="hover:text-gray-300">About</Link>
-          <Link to="/contact" className="hover:text-gray-300">Contact</Link>
+          <Link to="/about" className="hover:text-gray-300">About Us</Link>
+          <Link to="/contact" className="hover:text-gray-300">Contact Us</Link>
         </div>
       </div>
     </nav>
--- client/src/pages/ProjectsPage.js
+++ client/src/pages/ProjectsPage.js
@@ -1,27 +1,29 @@
-// client/src/pages/ProjectsPage.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ProjectCard from "../components/ProjectCard";
 
-const PORT = process.env.PORT;
-
 function ProjectsPage() {
   const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
-    axios.get(`http://localhost:${5000}/api/projects`)
+    axios.get("/api/projects")  
       .then(response => {
         setProjects(response.data);
+        setLoading(false);
       })
       .catch(error => {
         console.error("Error fetching projects:", error);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div>
-      {projects.length === 0 ? (
-        <p>No projects found.</p>
+      {loading ? (
+        <p>Loading projects...</p>   // Show while fetching
+      ) : projects.length === 0 ? (
+        <p>No projects found.</p>    // Show only if loading complete and empty
       ) : (
         projects.map(project => (
           <ProjectCard key={project._id} project={project} />
