58ed43747c206895c72e3d6dd9d0d68f6ab121c6
Connor Buckley <connorbuckley144@gmail.com>
2025-07-31 15:35:56 -0700

    Update CORS url

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://construction-website-client.onrender.com"
+    "https://tbuckley-construction-website-client.onrender.com"
   ]
 }));
 
